c11594f2dfd6ca5710819363afac8e23dc10d843
Calvin Kern <ckern@pixio.com>
2018-02-03 23:25:58 -0700

    Made user ship bigger for better UX

--- Stardroid/app/src/main/java/com/seakernel/stardroid/model/controller/ship/UserShip.java
+++ Stardroid/app/src/main/java/com/seakernel/stardroid/model/controller/ship/UserShip.java
@@ -11,14 +11,26 @@ import com.seakernel.stardroid.model.controller.weapon.Gun;
 public class UserShip extends BaseShip {
     private static final float ENGINE_SPEED_PERCENT = 20.0f;
 
+
+    @Override
+    protected float[] getCoordinates() {
+        final float size = 0.1f;
+        return new float[] {
+                -size, -size, 0.0f, // bottom left
+                size, -size, 0.0f,  // bottom right
+                -size,  size, 0.0f, // top left
+                size,  size, 0.0f,  // top right
+        };
+    }
+
     public UserShip() {
         super();
 
         setColor(OpenGlColors.PLAYER_BLUE);
         setCanShoot(true);
         setEngineSpeed(ENGINE_SPEED_PERCENT);
-        addGun(new Gun(0, getHeight() / 2, 0, null, 1000f));
-        addGun(new Gun(0, -getHeight() / 2, 0, null, 1000f));
+        addGun(new Gun(getWidth() / 2, getHeight() / 6, 0, null, 500.0f));
+        addGun(new Gun(getWidth() / 2, -getHeight() / 6, 0, null, 500.0f));
     }
 
 }
